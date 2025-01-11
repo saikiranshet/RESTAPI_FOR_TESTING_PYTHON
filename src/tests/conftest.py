856891c8a7eee178237e50bbaa3de25a2157e496
saikiranshet <32921574+saikiranshet@users.noreply.github.com>
2025-01-11 16:35:35 +0000

    checkincode

---
+++ src/tests/conftest.py
@@ -0,0 +1,20 @@
+# from base64 import b64encode
+import pytest
+import os
+from dotenv import load_dotenv 
+from ..utils.request_gen import APICaller
+
+
+
+load_dotenv()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def get_api_obj():
+    BASE_URL = os.getenv("BASE_URL")
+    # API_ID = os.getenv("API_ID")
+    # API_TOKEN = os.getenv("API_TOKEN")
+    # token = b64encode(f"{API_ID}:{API_TOKEN}".encode('utf-8')).decode("ascii")
+    headers = {"Connection": f"keep-alive"}
+    api_obj = APICaller(BASE_URL,headers)
+    return api_obj
--- src/tests/test_free_api.py
+++ src/tests/test_free_api.py
@@ -3,16 +3,60 @@ from ..utils.routes import USER_INFO
 from ..utils import DATA
 import pytest
 
-
 class TestUserInfo:
     @pytest.mark.smoke
     @pytest.mark.regression
-    def test_fetch(self, get_api_obj: APICaller):
+    def test_object(self, get_api_obj: APICaller):
         """
         This test is to fetch info
         """
         url_offset = USER_INFO.GET_INFO
         data = get_api_obj.get(url_offset)
-        print(data)
-       
-       
+        first_item = data[0]
+        id_name = (first_item['id'], first_item['name'])
+        expected = ('1', 'Google Pixel 6 Pro')
+        assert id_name == expected, f"Expected {expected}, but got {id_name}"
+
+
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_object_with_particularid(self, get_api_obj: APICaller):
+        """
+        This test is to fetch info
+        """
+        url_offset = USER_INFO.GET_PARTICULAR_ID
+        data = get_api_obj.get(url_offset)
+        third_item = data[0]
+        id_name = (third_item['id'], third_item['name'])
+        expected = ('3','Apple iPhone 12 Pro Max')
+        assert id_name == expected, f"Expected {expected}, but got {id_name}"
+
+    
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_object_with_particularids(self, get_api_obj: APICaller):
+        """
+        This test is to fetch info
+        """
+        url_offset = USER_INFO.GET_PARTICULAR_ID
+        data = get_api_obj.get(url_offset)
+        id_name_pairs = [(item['id'], item['name']) for item in data if item['id'] in ['3', '5', '10']]
+        expected_pairs = [
+              ('3', 'Apple iPhone 12 Pro Max'),
+               ('5', 'Samsung Galaxy Z Fold2'),
+             ('10', 'Apple iPad Mini 5th Gen')
+            ]       
+        for id_name, expected in zip(id_name_pairs, expected_pairs):
+             assert id_name == expected, f"Expected {expected}, but got {id_name}"
+
+           
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_object_with_objectvalue(self, get_api_obj: APICaller):
+        """
+        This test is to fetch info
+        """
+        url_offset = USER_INFO.GET_PARTICULAR_VAL
+        data = get_api_obj.get(url_offset)
+        id_value = data['id']
+        assert id_value == '7', f"Expected '7', but got {id_value}"
