10b0faadcd2945151d068d991dbd5dbe359055e7
saikiranshet <32921574+saikiranshet@users.noreply.github.com>
2025-01-24 02:24:14 +0000

    Small Understandbale

--- src/tests/test_free_api.py
+++ src/tests/test_free_api.py
@@ -20,7 +20,7 @@ class TestUserInfo:
 
     @pytest.mark.smoke
     @pytest.mark.regression
-    def test_object_with_particularid(self, get_api_obj: APICaller):
+    def test_object_with_particularid(self, get_api_obj):
         """
         This test is to fetch info
         """
@@ -28,7 +28,7 @@ class TestUserInfo:
         data = get_api_obj.get(url_offset)
         third_item = data[0]
         id_name = (third_item['id'], third_item['name'])
-        expected = ('3','Apple iPhone 12 Pro Max')
+        expected = ('2','Apple iPhone 12 Pro Max')
         assert id_name == expected, f"Expected {expected}, but got {id_name}"
 
     
